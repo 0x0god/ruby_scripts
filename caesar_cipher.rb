4e76ff6c0ca59d0ae72d39d4a6c62a2e83cd8486
Gabriel Mogaji 'R <37302024+0x0god@users.noreply.github.com>
2023-10-26 18:32:39 +0100

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,24 +1,17 @@
-# frozen_string_literal: true
-
-def caesar_cipher(text, shift)
-  (0...text.length).each do |i|
-    char_code = text[i].ord
-
-    (a, z) = case char_code
-             when 97..122 then [97, 122]   # a-z
-             when 65..90  then [65, 90]    # A-Z
-             else next
-             end
-
-    rotate = shift.positive? ? 26 : -26
-
-    char_code += shift
-    char_code -= rotate unless char_code.between?(a, z)
-
-    text[i] = char_code.chr
+def caesar_cipher(string, num)
+  alphabet = [*'a'..'z', *'A'..'Z']
+  caesar = ""
+  string.each_char do |letter|
+    old_idx = alphabet.find_index(letter)
+    if old_idx.nil?
+      caesar += letter
+    else
+      new_idx = (old_idx + num) % alphabet.count
+      caesar += alphabet[new_idx]
+    end
   end
+
+  caesar
 end
 
-message = 'Omae wa mou shindeiru. -NANI!?!!'
-
-caesar_cipher(message, 21)
+puts caesar_cipher("lorem ipsum",14) 
